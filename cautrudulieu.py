75923cee4d4698f5216782526fcf9177bf33e4af
chuvitvang <152381244+chuvitvang@users.noreply.github.com>
2024-11-28 00:26:32 +0700

    Update cautrudulieu.py

--- cautrudulieu.py
+++ cautrudulieu.py
@@ -1,20 +1,21 @@
+#tạo 1 dictionary với các key A,B,C
 loikhai = {
     'A': input('Nhập lời khai của A: '),
     'B': input('Nhập lời khai của B: '),
     'C': input('Nhập lời khai của C: ')
 }
 #cả 3 người đều im lăng và 3 người đều tố cáo
-if all(value == 'slient' for value in loikhai.values()):
+if all(value == 'slient' for value in loikhai.values()): #hàm all() kiểm tra tất cả các values(giá trị)
     print('A, B, C lãnh án 5 năm.')
 else:
     print('A, B, C lãnh án 10 năm.')
 
-for person, statement in loikhai.items():
+for nguoi, statement in loikhai.items(): # hàm items() lấy cả key và giá trị
     # Kiểm tra trường hợp có 2 người tố cáo, 1 người giữ im lặng
-    if sum(value != 'slient' for value in loikhai.values()) == 2 and statement == 'slient':
-        print(f'{person} lãnh án 20 năm, các người còn lại lãnh án 10 năm.')
+    if sum(value != 'slient' for value in loikhai.values()) == 2 and statement == 'slient': #đk tổng số người tố cáo =2 ,chỉ xét người im lặng là statement
+        print(f'{nguoi} lãnh án 20 năm, các người còn lại lãnh án 10 năm.')
         break
     # Kiểm tra trường hợp 1 người tố cáo và 2 người im lặng
-    elif sum(value != 'tocao' for value in loikhai.values()) == 2 and statement == 'tocao':
-        print(f'{person} tự do, các người còn lại lãnh án 10 năm.')
+    elif sum(value != 'tocao' for value in loikhai.values()) == 2 and statement == 'tocao': #đk tổng số người im lặng =2 ,chỉ xét người tố cáo là statement
+        print(f'{nguoi} tự do, các người còn lại lãnh án 10 năm.')
         break
