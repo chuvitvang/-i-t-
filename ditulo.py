f1ee8de7d073d07ea456f76af0ae4275c8a52bf4
chuvitvang <152381244+chuvitvang@users.noreply.github.com>
2024-11-27 23:59:26 +0700

    Update ditulo.py

--- ditulo.py
+++ ditulo.py
@@ -2,31 +2,32 @@ loikhai_A = input('Nhập lời khai của A: ')
 loikhai_B = input('Nhập lời khai của B: ')
 loikhai_C = input('Nhập lời khai của C: ')
 
+#cả 3 người đều im lặng
 if loikhai_A == loikhai_B == loikhai_C == 'slient':
     print('A, B, C lãnh án 5 năm.')
-elif loikhai_A == 'tocaoC' and loikhai_B == 'tocaoC':
-    print('A, B lãnh án 10 năm, C lãnh án 20 năm.')
-elif loikhai_B == 'tocaoA' and loikhai_C == 'tocaoA':
-    print('B, C lãnh án 10 năm, A lãnh án 20 năm.')
-elif loikhai_A == 'tocaoB' and loikhai_C == 'tocaoB':
-    print('A, C lãnh án 10 năm, B lãnh án 20 năm.')
-elif loikhai_A == 'slient' and loikhai_B != 'slient' and loikhai_C != 'slient':
-    print('B, C lãnh án 10 năm, A lãnh án 20 năm.')
-elif loikhai_B == 'slient' and loikhai_A != 'slient' and loikhai_C != 'slient':
-    print('A, C lãnh án 10 năm, B lãnh án 20 năm.')
-elif loikhai_C == 'slient' and loikhai_A != 'slient' and loikhai_B != 'slient':
-    print('A, B lãnh án 10 năm, C lãnh án 20 năm.')
-elif loikhai_A == 'tocaoC':
-    print('A tự do, B và C lãnh án 10 năm.')
-elif loikhai_B == 'tocaoC':
-    print('B tự do, A và C lãnh án 10 năm.')
-elif loikhai_B == 'tocaoA':
-    print('B tự do, A và C lãnh án 10 năm.')
-elif loikhai_C == 'tocaoA':
-    print('C tự do, A và B lãnh án 10 năm.')
-elif loikhai_A == 'tocaoB':
-    print('A tự do, B và C lãnh án 10 năm.')
-elif loikhai_C == 'tocaoB':
-    print('C tự do, A và B lãnh án 10 năm.')
-else:
+#cả 3 người đều tố cáo
+elif loikhai_A == loikhai_B == loikhai_C != 'slient':
     print('A, B, C lãnh án 10 năm.')
+# Kiểm tra trường hợp có 2 người tố cáo, 1 người giữ im lặng
+#3 truong hop:
+#a b to cao c im
+#a c to cao b im
+#b c to cao a im
+elif loikhai_A == loikhai_B =='tocao':
+    print('C lãnh án 20 năm , A B lãnh án 10 năm')
+elif loikhai_A == loikhai_C =='tocao':
+    print('B lãnh án 20 năm , A C lãnh án 10 năm')
+elif loikhai_B == loikhai_C =='tocao':
+    print('A lãnh án 20 năm , B C lãnh án 10 năm')
+# Kiểm tra trường hợp có 1 người tố cáo, 2 người giữ im lặng
+#3 truong hop:
+#a b im c to cao
+#a c im b to cao
+#b c im a to cao
+elif loikhai_A == loikhai_B =='slient':
+    print('C tự do , A B lãnh án 10 năm')
+elif loikhai_A == loikhai_C =='slient':
+    print('B tự do , A C lãnh án 10 năm')
+elif loikhai_B == loikhai_C =='slient':
+    print('A tự do , B C lãnh án 10 năm')
+
