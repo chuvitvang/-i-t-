2f1d6e18d66fd66f2d16217c5b96439d81574311
chuvitvang <152381244+chuvitvang@users.noreply.github.com>
2024-11-27 23:54:58 +0700

    Update cautrudulieu.py

--- cautrudulieu.py
+++ cautrudulieu.py
@@ -9,15 +9,12 @@ if all(value == 'slient' for value in loikhai.values()):
 #cả 3 người đều tố cáo
 elif all(value != 'slient' for value in loikhai.values()):
     print('A, B, C lãnh án 10 năm.')
-# Kiểm tra trường hợp có 2 người tố cáo, 1 người giữ im lặng
-elif sum(value != 'slient' for value in loikhai.values()) == 2:
-    for person, statement in loikhai.items():
-        if statement == 'slient':
-            print(f'{person} lãnh án 20 năm, các người còn lại lãnh án 10 năm.')
+for person, statement in loikhai.items():
+    # Kiểm tra trường hợp có 2 người tố cáo, 1 người giữ im lặng
+    if sum(value != 'slient' for value in loikhai.values()) == 2 and statement == 'slient':
+        print(f'{person} lãnh án 20 năm, các người còn lại lãnh án 10 năm.')
         break
-# Kiểm tra trường hợp 1 người tố cáo và 2 người im lặng
-elif sum(value != 'tocao' for value in loikhai.values()) == 2:
-    for person, statement in loikhai.items():
-        if statement == 'tocao':
-            print(f'{person} tự do, các người còn lại lãnh án 10 năm.')
+    # Kiểm tra trường hợp 1 người tố cáo và 2 người im lặng
+    elif sum(value != 'tocao' for value in loikhai.values()) == 2 and statement == 'tocao':
+        print(f'{person} tự do, các người còn lại lãnh án 10 năm.')
         break
